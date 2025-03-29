dbef25a0633c2c8b704e8ac6b23fecf1bb0b2581
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2025-03-29 10:01:21 +0100

    Lint entire app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
-import {provideExperimentalZonelessChangeDetection} from "@angular/core";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideExperimentalZonelessChangeDetection()]
-})
-  .catch((err) => console.error(err));
+  providers: [provideExperimentalZonelessChangeDetection()],
+}).catch((err) => console.error(err));
